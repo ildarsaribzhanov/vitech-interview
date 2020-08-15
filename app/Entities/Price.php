31b243701331ae335723f9b10f3dd31f07ef7f62
Ildar Saribzhanov <ildar.r.saribzhanov@gmail.com>
2020-08-15 15:25:11 +0300

    [Add] price entity

---
+++ app/Entities/Price.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Entities;
+
+
+use DomainException;
+
+/**
+ * Class Price
+ *
+ * @package App\Entities
+ */
+class Price
+{
+    /** @var int */
+    private int $price;
+
+    /** Count symbols after comma */
+    private const AFTER_COMMA = 2;
+
+    /**
+     * Price constructor.
+     *
+     * @param int $price
+     */
+    private function __construct(int $price)
+    {
+        if ($price < 1) {
+            throw new DomainException('Price must be more then 0.01');
+        }
+
+        $this->price = $price;
+    }
+
+    /**
+     * @param float $val
+     *
+     * @return Price
+     */
+    public static function create(float $val): Price
+    {
+        return new self($val * (10 ** self::AFTER_COMMA));
+    }
+
+    /**
+     * @return float
+     */
+    public function getVal(): float
+    {
+        return round($this->price / 10 ** self::AFTER_COMMA, 2);
+    }
+
+    /**
+     * @param Price $price
+     *
+     * @return Price
+     */
+    public function add(Price $price): Price
+    {
+        $priceVal = $price->getVal() * (10 ** self::AFTER_COMMA);
+
+        return new Price($this->price + $priceVal);
+    }
+}
---
+++ tests/Entities/PriceTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Tests\Entities;
+
+use App\Entities\Price;
+use DomainException;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class PriceTest
+ *
+ * @package Tests\Entities
+ */
+class PriceTest extends TestCase
+{
+    /**
+     * Test base create
+     */
+    public function testCreate()
+    {
+        $price = Price::create(0.01);
+        $this->assertEquals(0.01, $price->getVal());
+
+        $price = Price::create(1);
+        $this->assertEquals(1, $price->getVal());
+    }
+
+    /**
+     * Text check exception
+     */
+    public function testCreatedException()
+    {
+        $this->expectException(DomainException::class);
+        Price::create(-1);
+    }
+
+    /**
+     * Text check exception
+     */
+    public function testCreatedExceptionSmallest()
+    {
+        $this->expectException(DomainException::class);
+        Price::create(0.001);
+    }
+
+    /**
+     * Test added price
+     */
+    public function testAdd()
+    {
+        $price = Price::create(3);
+        $this->assertEquals(Price::create(3), $price);
+        $this->assertEquals(Price::create(7), $price->add(Price::create(4)));
+    }
+}
