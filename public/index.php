baf8709270f20d91bd1f8c76ba6ac5605d0fc0a6
Ildar Saribzhanov <ildar.r.saribzhanov@gmail.com>
2020-08-16 10:49:28 +0300

    [Add] env and dotenv package

--- public/index.php
+++ public/index.php
@@ -1,7 +1,13 @@
 <?php
+
+use Dotenv\Dotenv;
+
 define('APP', dirname(__DIR__));
 
 chdir(APP);
 require 'vendor/autoload.php';
+
+$dotenv = Dotenv::createImmutable(APP);
+$dotenv->load();
 
 require "router/routes.php";
